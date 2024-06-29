7b44141dbfdb09a25d1a2272eebd6882ea56695b
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2024-06-29 18:10:29 +0530

    Initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const saltRounds = 10;
 env.config()
 const db = new pg.Client({
     user: process.env.PG_USER,
-    host: process.env.PG_HOST,
+    host: process.env.PG_HOST || "localhost",
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
