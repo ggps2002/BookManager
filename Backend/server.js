b06e243504145ea2e6352ca59008859d4f2a3813
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2024-06-30 17:48:29 +0530

    changed cors origin

--- Backend/server.js
+++ Backend/server.js
@@ -45,7 +45,7 @@ app.use(session({
 }))
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.ORIGIN || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
