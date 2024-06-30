93b54d359e8b06c7b192a53961ceb4eb5f87e2f9
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2024-06-30 19:23:47 +0530

    deployment sucks

--- Backend/server.js
+++ Backend/server.js
@@ -46,7 +46,7 @@ app.use(session({
 }))
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.ORIGIN || 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
